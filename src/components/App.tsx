0705051ca375c3bca6b502c2249776bc288199eb
macinjoke <shunji.makino@gmail.com>
2023-05-25 18:15:07 +0900

    なんとなくルーレットするところまで。音付き

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,39 @@
-import { FC } from 'react'
+import { FC, useCallback, useState } from 'react'
 
-import megaphonePng from '@/assets/imgs/megaphone.png'
+import hitokoto1Png from '@/assets/imgs/hitokoto_1.jpeg'
+import hitokoto2Png from '@/assets/imgs/hitokoto_2.jpeg'
+
+import styles from './App.module.css'
+
+const hitokotoList = [hitokoto1Png, hitokoto2Png]
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max)
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = () => {
+  const onStartClick = useCallback(() => {
+    console.log('onStartClick')
+    setImageIndex(1)
+    const id = window.setInterval(() => {
+      const index = getRandomInt(hitokotoList.length)
+      setImageIndex(index)
+      const piAudio = new Audio('/pi.mp3')
+      piAudio.play()
+    }, 500)
+  }, [])
+
+  const [imageIndex, setImageIndex] = useState<number | null>(null)
+
   return (
-    <div>
-      aa
-      <div>
-        <img src={megaphonePng} />
-      </div>
+    <div className={styles.root}>
+      {imageIndex !== null && (
+        <div>
+          <img src={hitokotoList[imageIndex]} />
+        </div>
+      )}
+      <button onClick={onStartClick}>Start!</button>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,20 @@ import svgr from 'vite-plugin-svgr'
 
 import react from '@vitejs/plugin-react'
 
-import { envKeyMap } from './src/globals/config'
+// import { envKeyMap } from './src/globals/config'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
 
-  console.log('Public environment variables')
+  // console.log('Public environment variables')
   // 環境変数のバリデーション
-  for (const key of Object.values(envKeyMap)) {
-    console.log(`${key}: ${env[key]}`)
-    if (env[key] === undefined) {
-      throw Error(`Environment variables are insufficient. key: ${key}`)
-    }
-  }
+  // for (const key of Object.values(envKeyMap)) {
+  //   console.log(`${key}: ${env[key]}`)
+  //   if (env[key] === undefined) {
+  //     throw Error(`Environment variables are insufficient. key: ${key}`)
+  //   }
+  // }
 
   return {
     resolve: {
